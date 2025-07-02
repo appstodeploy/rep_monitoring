8978b2c130cdecb5ecdb3117afdca2cd38a6e0f8
Erfan Moeini <seotabdeal@gmail.com>
2025-07-02 13:39:37 +0330

    commit  checklinks branch to main branch

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import tldextract
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+                "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 }
 
 # --- Load Google Sheet ---
